e384c80246993e8d2d22e5c9f07406e1474decba
Alisa Tran <a43tran@uwaterloo.ca>
2026-08-05 09:16:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2461,6 +2461,14 @@ function keyPressed() {
     if (gameOver) restartGame();
   }
 
+  if (key === "e" || key === "E") {
+    if (thirdLevelComplete) restartGame();
+  }
+  
+  if (key === "m" || key === "M") {
+    loadThirdLevel();
+  }
+
 if (key === "n" || key === "N") {
     if (firstLevelComplete) {
       loadSecondLevel();
@@ -4371,6 +4379,9 @@ function drawLoseScreen() {
 }
 
 function restartGame() {
+  // Preserve whether level 3 had all food collected before resetting
+  let hadAllFoodBeforeRestart = maze === maze3 && (level3AllFoodCollected || collectedCount === collectibles.length);
+
   socialBattery = 100;
 
   gameOver = false;
@@ -4431,6 +4442,13 @@ function restartGame() {
     resetBeakers();
 
     resetLevel3State();
+    // If the player had already collected all food before dying, restart the chase immediately
+    if (hadAllFoodBeforeRestart) {
+      level3AllFoodCollected = true;
+      startLevel3Chase();
+      level3FoodCollectedDialogueActive = true;
+      level3FoodCollectedDialogueIndex = 0;
+    }
   }
 
   // Return player to the current level's start tile
